c7b8d3fbb8fc8790ae25bd16ff4945456a13c70f
vanhieufw <nguyenvanhieu206fw@gmail.com>
2025-05-17 19:57:00 +0700

    Update MovieDAO.java

--- MovieDAO.java
+++ MovieDAO.java
@@ -3,21 +3,25 @@ package com.movie.dao;
 import com.movie.model.Movie;
 import com.movie.util.DBConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MovieDAO {
     public void addMovie(Movie movie) throws SQLException {
-        String query = "INSERT INTO Movie (Title, Description, Poster) VALUES (?, ?, ?)";
+        String query = "INSERT INTO Movie (Title, Description, Duration, Director, GenreID, Poster, StartDate, EndDate, ProductionYear, CountryID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
             stmt.setString(1, movie.getTitle());
             stmt.setString(2, movie.getDescription());
-            stmt.setString(3, movie.getPoster());
+            stmt.setInt(3, movie.getDuration());
+            stmt.setString(4, movie.getDirector());
+            stmt.setInt(5, movie.getGenreID());
+            stmt.setString(6, movie.getPoster());
+            stmt.setDate(7, movie.getStartDate());
+            stmt.setDate(8, movie.getEndDate());
+            stmt.setInt(9, movie.getProductionYear());
+            stmt.setInt(10, movie.getCountryID());
             stmt.executeUpdate();
             try (ResultSet rs = stmt.getGeneratedKeys()) {
                 if (rs.next()) {
@@ -31,7 +35,9 @@ public class MovieDAO {
 
     public List<Movie> getAllMovies() throws SQLException {
         List<Movie> movies = new ArrayList<>();
-        String query = "SELECT * FROM Movie";
+        String query = "SELECT m.*, g.GenreName, c.CountryName FROM Movie m " +
+                "LEFT JOIN Genre g ON m.GenreID = g.GenreID " +
+                "LEFT JOIN Country c ON m.CountryID = c.CountryID";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(query);
              ResultSet rs = stmt.executeQuery()) {
@@ -40,7 +46,16 @@ public class MovieDAO {
                 movie.setMovieID(rs.getInt("MovieID"));
                 movie.setTitle(rs.getString("Title"));
                 movie.setDescription(rs.getString("Description"));
+                movie.setDuration(rs.getInt("Duration"));
+                movie.setDirector(rs.getString("Director"));
+                movie.setGenreID(rs.getInt("GenreID"));
+                movie.setGenreName(rs.getString("GenreName"));
                 movie.setPoster(rs.getString("Poster"));
+                movie.setStartDate(rs.getDate("StartDate"));
+                movie.setEndDate(rs.getDate("EndDate"));
+                movie.setProductionYear(rs.getInt("ProductionYear"));
+                movie.setCountryID(rs.getInt("CountryID"));
+                movie.setCountryName(rs.getString("CountryName"));
                 movies.add(movie);
             }
         } catch (SQLException e) {
@@ -50,7 +65,10 @@ public class MovieDAO {
     }
 
     public Movie getMovieById(int movieID) throws SQLException {
-        String query = "SELECT * FROM Movie WHERE MovieID = ?";
+        String query = "SELECT m.*, g.GenreName, c.CountryName FROM Movie m " +
+                "LEFT JOIN Genre g ON m.GenreID = g.GenreID " +
+                "LEFT JOIN Country c ON m.CountryID = c.CountryID " +
+                "WHERE m.MovieID = ?";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setInt(1, movieID);
@@ -60,7 +78,16 @@ public class MovieDAO {
                     movie.setMovieID(rs.getInt("MovieID"));
                     movie.setTitle(rs.getString("Title"));
                     movie.setDescription(rs.getString("Description"));
+                    movie.setDuration(rs.getInt("Duration"));
+                    movie.setDirector(rs.getString("Director"));
+                    movie.setGenreID(rs.getInt("GenreID"));
+                    movie.setGenreName(rs.getString("GenreName"));
                     movie.setPoster(rs.getString("Poster"));
+                    movie.setStartDate(rs.getDate("StartDate"));
+                    movie.setEndDate(rs.getDate("EndDate"));
+                    movie.setProductionYear(rs.getInt("ProductionYear"));
+                    movie.setCountryID(rs.getInt("CountryID"));
+                    movie.setCountryName(rs.getString("CountryName"));
                     return movie;
                 }
             }
@@ -71,13 +98,20 @@ public class MovieDAO {
     }
 
     public void updateMovie(Movie movie) throws SQLException {
-        String query = "UPDATE Movie SET Title = ?, Description = ?, Poster = ? WHERE MovieID = ?";
+        String query = "UPDATE Movie SET Title = ?, Description = ?, Duration = ?, Director = ?, GenreID = ?, Poster = ?, StartDate = ?, EndDate = ?, ProductionYear = ?, CountryID = ? WHERE MovieID = ?";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, movie.getTitle());
             stmt.setString(2, movie.getDescription());
-            stmt.setString(3, movie.getPoster());
-            stmt.setInt(4, movie.getMovieID());
+            stmt.setInt(3, movie.getDuration());
+            stmt.setString(4, movie.getDirector());
+            stmt.setInt(5, movie.getGenreID());
+            stmt.setString(6, movie.getPoster());
+            stmt.setDate(7, movie.getStartDate());
+            stmt.setDate(8, movie.getEndDate());
+            stmt.setInt(9, movie.getProductionYear());
+            stmt.setInt(10, movie.getCountryID());
+            stmt.setInt(11, movie.getMovieID());
             int rowsAffected = stmt.executeUpdate();
             if (rowsAffected == 0) {
                 throw new SQLException("No movie found with ID " + movie.getMovieID());
@@ -99,5 +133,63 @@ public class MovieDAO {
         } catch (SQLException e) {
             throw new SQLException("Failed to delete movie: " + e.getMessage(), e);
         }
+    }
+
+    public List<String> getAllGenres() throws SQLException {
+        List<String> genres = new ArrayList<>();
+        String query = "SELECT GenreName FROM Genre";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                genres.add(rs.getString("GenreName"));
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Failed to retrieve genres: " + e.getMessage(), e);
+        }
+        return genres;
+    }
+
+    public List<String> getAllCountries() throws SQLException {
+        List<String> countries = new ArrayList<>();
+        String query = "SELECT CountryName FROM Country";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                countries.add(rs.getString("CountryName"));
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Failed to retrieve countries: " + e.getMessage(), e);
+        }
+        return countries;
+    }
+
+    public int getGenreIdByName(String genreName) throws SQLException {
+        String query = "SELECT GenreID FROM Genre WHERE GenreName = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, genreName);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("GenreID");
+                }
+            }
+        }
+        return 0;
+    }
+
+    public int getCountryIdByName(String countryName) throws SQLException {
+        String query = "SELECT CountryID FROM Country WHERE CountryName = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, countryName);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("CountryID");
+                }
+            }
+        }
+        return 0;
     }
 }
